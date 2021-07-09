25786869842716aab2175a0672ef936dd36aa956
Reock1020 <reock1020@icloud.com>
2021-07-09 11:22:37 +0900

    Non-overlapping issues

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const output_svg = document.getElementById('questions');
 
 let count = 0;
 
+
 function csv_data(dataPath) {
     const request = new XMLHttpRequest(); // HTTPでファイルを読み込む
     request.addEventListener('load', (event) => { // ロードさせ実行
@@ -22,21 +23,20 @@ function csv_array(data) {
 }
 
 function flash_question(dataArray, dataString) {
-
-    const question_num = Math.floor(Math.random() * (dataString.length));
+    const len = dataString.length;
+    const question_num = getRandom(len);
     localStorage.setItem('value', question_num);
     const question_volume = dataArray[question_num].length;
-
     const interValid = setInterval(() => {
         if (count >= question_volume) {
             window.location.href = 'choice.html';
             clearInterval(interValid);
         }
 
-        if(count < question_volume) {
+        if (count < question_volume) {
             print(dataArray[question_num]);
             count++;
-        } 
+        }
     }, 3000);
 
 }
@@ -44,6 +44,39 @@ function flash_question(dataArray, dataString) {
 function print(data) {
     output_svg.innerHTML = data[count];
     console.log(count);
+}
+
+function getRandom(len) {
+    let data = [];
+    data[0] = 0;
+    data = sessionStorage.getItem('array');
+    let value;
+    let count;
+    console.log(data);
+    if (data.length > 0) {
+        while (true) {
+            count = 0;
+            value = Math.floor(Math.random() * (len));
+            for (let i = 0; i < data.length; i++) {
+                if (data[i] == value) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                break;
+            }
+        }
+    }
+    if (data.length < 1) {
+        value = Math.floor(Math.random() * (len));
+        data[0] = value;
+        sessionStorage.setItem('array', data);
+    } else {
+        data.push(value);
+        sessionStorage.setItem('array', data);
+    }
+    console.log(data);
+    return value;
 }
 
 csv_data('mondai.csv'); // csvのパス
